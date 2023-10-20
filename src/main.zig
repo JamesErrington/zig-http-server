2e523bf1b106967e13bd4bd7da33ffc8c332df26
James Errington <james.errington@protonmail.com>
2023-10-20 16:14:49 +0100

    Remove hardcoded sockaddr

--- src/main.zig
+++ src/main.zig
@@ -1,22 +1,23 @@
 const std = @import("std");
 const os = std.os;
-
-const addr = os.sockaddr{.family = 2, .data = [14]u8{ 0x22, 0xb8, 0x7f, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }};
+const assert = std.debug.assert;
 
 pub fn main() !void {
-    var socket = try os.socket(2, 1, 6);
+    var sockfd = try os.socket(2, 1, 6);
+    defer os.close(sockfd);
     
-    try os.bind(socket, &addr, @sizeOf(os.sockaddr));
+    const addr = try make_sockaddr("127.0.0.1", 8888);
+    try os.bind(sockfd, &addr, @sizeOf(os.sockaddr));
     std.log.info("Socket bound!", .{});
 
-    try os.listen(socket, 5);
+    try os.listen(sockfd, 5);
     std.log.info("Socket listening!", .{});
-
 
     while (true) {
         var incoming_addr: ?*os.sockaddr = null;
         var addr_size: ?*os.socklen_t = null;
-        const connfd = try os.accept(socket, incoming_addr, addr_size, 0);
+        const connfd = try os.accept(sockfd, incoming_addr, addr_size, 0);
+        defer os.close(connfd);
         std.log.info("Connection accepted!", .{});
 
         var buffer: [1024]u8 = undefined;
@@ -24,8 +25,34 @@ pub fn main() !void {
         std.log.info("Read {} bytes:", .{bytes});
         std.debug.print("{s}\n", .{buffer[0..bytes]});
 
-        _ = try os.write(connfd, &buffer);
-        
-        os.close(connfd);
+        _ = try os.write(connfd, &buffer);   
     }
+}
+
+// os.sockaddr{.family = 2, .data = [14]u8{ 0x22, 0xb8, 0x7f, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }}
+fn make_sockaddr(host: []const u8, port: u16) !os.sockaddr {
+    var addr = os.sockaddr{
+        .family = 2,
+        .data = undefined,
+    };
+
+    const bytes = std.mem.toBytes(port);
+    comptime assert(bytes.len == 2);
+    // Swap bytes to be the right order
+    // @FIXME: this probably isn't compatible everywhere
+    addr.data[0] = bytes[1];
+    addr.data[1] = bytes[0];
+
+    var s: usize = 0; var idx: usize = 2;
+    for (host, 0..) |char, i| {
+        if (char == '.' or i == host.len-1) {
+            const end = if (i == host.len-1) i + 1 else i;
+            const byte = try std.fmt.parseInt(u8, host[s..end], 10);
+            addr.data[idx] = byte;
+            s = i + 1; idx += 1;
+        }
+    }
+
+    std.debug.print("{any}\n", .{addr.data});
+    return addr;
 }
